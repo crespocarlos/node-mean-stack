e21bfe4c8c6d722e841bde0f29272b626def9a77
Carlos Crespo <carlos.hcrespo@gmail.com>
2016-05-24 14:24:58 -0300

    improving UI

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,12 @@
-angular.module('app', ['ngResource', 'ngRoute']);
-angular.module('app').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $routeProvider
-        .when('/', { templateUrl: '/partials/main', controller: 'mainCtrl' })
+(function () {
+    'use strict';
+    
+    angular.module('app', ['ngResource', 'ngRoute']);
 
-}]);
+    angular.module('app').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', { templateUrl: '/partials/main', controller: 'mvMainCtrl' })
 
-angular.module('app').controller('mainCtrl', function ($scope) {
-    $scope.myVar = 'Hello';
-});
+    }]);
+})();
